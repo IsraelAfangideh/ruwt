147a6588f1028193ecaec1205de732896e52b1b1
Israel Afangideh <105407225+IsraelAfangideh@users.noreply.github.com>
2025-11-08 13:03:18 +0100

    Add auth_provider table Email can now be null human_number is an Integer

--- migrations/1.sql
+++ migrations/1.sql
@@ -1,6 +1,6 @@
 CREATE TABLE human (
                    number     INTEGER PRIMARY KEY,
-                   email      TEXT UNIQUE NOT NULL,
+                   email      TEXT UNIQUE,
                    created_at INTEGER DEFAULT (unixepoch())
 );
 
@@ -10,9 +10,17 @@ CREATE TABLE runner (
                     created_at INTEGER DEFAULT (unixepoch())
 );
 
+CREATE TABLE auth_provider (
+                           number                    INTEGER PRIMARY KEY,
+                           human_number              INTEGER NOT NULL REFERENCES human (number) ON DELETE CASCADE,
+                           provider_name             TEXT    NOT NULL,
+                           provider_human_identifier TEXT    NOT NULL,
+                           UNIQUE (provider_name, provider_human_identifier)
+);
+
 CREATE TABLE session (
                      number       INTEGER PRIMARY KEY,
-                     human_number TEXT    NOT NULL REFERENCES human (number) ON DELETE CASCADE,
+                     human_number INTEGER NOT NULL REFERENCES human (number) ON DELETE CASCADE,
                      expires_at   INTEGER NOT NULL
 );
 
